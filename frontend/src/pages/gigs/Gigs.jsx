7177a666d4e46ab474f6612b363d7886c35d54ee
Srinuchawan <chawansrinu574@gmail.com>
2026-02-20 16:04:22 +0530

    Final report

--- frontend/src/pages/gigs/Gigs.jsx
+++ frontend/src/pages/gigs/Gigs.jsx
@@ -43,10 +43,12 @@ function Gigs() {
   return (
     <div className="gigs">
       <div className="container">
-        <span className="breadcrumbs">Liverr > Graphics & Design ></span>
+        <span className="breadcrumbs">
+  Srinu {'>'} Graphics & Design {'>'}
+</span>
         <h1>AI Artists</h1>
         <p>
-          Explore the boundaries of art and technology with Liverr's AI artists
+          Explore the boundaries of art and technology with Srinu's AI artists
         </p>
         <div className="menu">
           <div className="left">

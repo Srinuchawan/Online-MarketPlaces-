8287daa3bd747aa028c31946c7320ed089f08b7e
Srinuchawan <chawansrinu574@gmail.com>
2026-02-20 14:56:24 +0530

    Finail testing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,7 +5,7 @@ export const deleteUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
   if (req.userId !== user._id.toString()) {
-    return next(createError(403, "You can delete only your account!"));
+    return next(createError(403, "You can delete only your Account!"));
   }
   await User.findByIdAndDelete(req.params.id);
   res.status(200).send("deleted.");

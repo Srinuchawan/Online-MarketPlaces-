2e53dd190a21fc807ccbc586aeb5726814d65ff5
Srinuchawan <chawansrinu574@gmail.com>
2026-02-20 14:50:05 +0530

    DAY 9:Integrate Stripe payment functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,7 +12,7 @@ export const register = async (req, res, next) => {
     });
 
     await newUser.save();
-    res.status(201).send("User has been created.");
+    res.status(201).send("User has been Created.");
   } catch (err) {
     next(err);
   }
